9ae2bb4434945399b82a95faa12337d4e6054895
Laysa <laysa.developer@gmail.com>
2024-07-18 09:45:36 -0300

    feat: add prisma schema and tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
+<script>
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <main class="flex min-h-screen bg-green-600">
+    <div class="mx-4 mt-4">
+      <h1 class="text-2xl font-bold">Hello World</h1>
+    </div>
+  </main>
 </template>
