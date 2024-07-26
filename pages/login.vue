3696f5f0dc6da4d8ed7dd43143b8ab920880b7d0
Laysa <laysa.developer@gmail.com>
2024-07-26 08:05:32 -0300

    refactor: removes unused shadcn/ui form and VeeValidate

--- pages/login.vue
+++ pages/login.vue
@@ -2,59 +2,61 @@
   <main class="flex min-h-screen flex-col items-center justify-center p-20">
     <div class="mx-auto w-full max-w-[500px] rounded-md border p-6">
       <h1 class="mb-6 text-center text-2xl">Login</h1>
-      <Form>
-        <FormField
-          v-slot="{ componentField }"
-          name="loginForm"
-        >
-          <FormItem>
-            <FormLabel />
-            <FormControl>
-              <Input placeholder="Seu nome" />
-              <Input placeholder="E-mail" />
-              <Input placeholder="Senha" />
-            </FormControl>
-            <div class="flex justify-end py-2">
-              <NuxtLink
-                to="/forgotpassword"
-                class="text-sm font-semibold"
-              >
-                Esqueceu sua senha?
-              </NuxtLink>
-            </div>
-            <div class="flex justify-center pt-2">
-              <Button
-                type="submit"
-                class="text-md w-full font-bold"
-              >
-                Entrar
-              </Button>
-            </div>
-            <div class="mx-6 pt-4">
-              <Separator label="OU" />
-            </div>
-            <div class="flex justify-center pt-4">
-              <Button
-                variant="outline"
-                class="min-w-[180px]"
-              >
-                <img
-                  src="/googleIcon.svg"
-                  alt="google icon"
-                  class="h-6 w-6"
-                />
-              </Button>
-            </div>
-          </FormItem>
-        </FormField>
-      </Form>
+      <form>
+        <Input
+          placeholder="E-mail"
+          required
+          id="email"
+          type="email"
+          v-model="user.email"
+          class="mb-1.5"
+        />
+        <Input
+          placeholder="Senha"
+          required
+          id="password"
+          type="password"
+          v-model="user.password"
+          class="mb-1"
+        />
+        <div>
+          <div class="flex justify-end pt-2 pb-6">
+            <NuxtLink
+              to="/forgotpassword"
+              class="text-sm font-semibold"
+            >
+              Esqueceu a senha?
+            </NuxtLink>
+          </div>
+          <Button
+            type="submit"
+            class="text-md w-full font-bold"
+            >Entrar</Button
+          >
+        </div>
+        <div class="mx-6 pt-6">
+          <Separator label="OU" />
+        </div>
+        <div class="flex justify-center pt-6">
+          <Button
+            variant="outline"
+            class="min-w-[175px]"
+          >
+            <img
+              src="/googleIcon.svg"
+              alt="google icon"
+              class="h-6 w-6"
+            />
+          </Button>
+        </div>
+      </form>
     </div>
   </main>
 </template>
 
 <script lang="ts" setup>
-import { FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { Separator } from '@/components/ui/separator'
-import { Button } from '@/components/ui/button'
+const user = ref({
+  email: '',
+  password: '',
+})
 </script>
