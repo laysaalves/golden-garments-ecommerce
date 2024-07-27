6f417f5779dbf764fad8c35b56ed028c24070cda
Laysa <laysa.developer@gmail.com>
2024-07-27 12:20:24 -0300

    feat: add shadcn/ui sheet to sidebar and cart components

---
+++ components/AppSidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="lg:hidden">
+    <Sheet>
+      <SheetTrigger as-child>
+        <Button
+          variant="outline"
+          size="icon"
+        >
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle>Menu</SheetTitle>
+          <Separator class="mt-3" />
+        </SheetHeader>
+        
+      </SheetContent>
+    </Sheet>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MenuIcon } from 'lucide-vue-next'
+</script>
---
+++ components/Cart.vue
@@ -0,0 +1,22 @@
+<template>
+  <Sheet>
+    <SheetTrigger as-child>
+      <Button
+        variant="outline"
+        size="icon"
+      >
+        <LucideShoppingCart />
+      </Button>
+    </SheetTrigger>
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Seu carrinho</SheetTitle>
+        <Separator class="mt-3" />
+      </SheetHeader>
+    </SheetContent>
+  </Sheet>
+</template>
+
+<script setup lang="ts">
+import { LucideShoppingCart } from 'lucide-vue-next'
+</script>
