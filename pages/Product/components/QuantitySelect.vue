a1f2f4e0061517363060aab2662ef5e4cda42c97
Laysa <laysa.developer@gmail.com>
2024-09-03 21:46:42 -0300

    feat(product pages): add shadcn-vue components to initial componentization

---
+++ pages/Product/components/QuantitySelect.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center gap-2 border-2 rounded-md">
+    <Button
+      variant="link"
+      class="size-12 p-0"
+    >
+      <Minus class="text-yellow-500 w-4 h-4" />
+    </Button>
+    <span>1</span>
+    <Button
+      variant="link"
+      class="size-12 p-0"
+    >
+      <Plus class="text-yellow-500 w-4 h-4" />
+    </Button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Minus, Plus } from 'lucide-vue-next'
+</script>
---
+++ pages/Product/components/SizeSelect.vue
@@ -0,0 +1,28 @@
+<template>
+  <RadioGroup
+    default-value="option-one"
+    class="flex flex-row justify-between items-center mb-2"
+  >
+    <div class="flex items-center space-x-2">
+      <RadioGroupItem
+        id="option-one"
+        value="option-one"
+      />
+      <Label for="option-one">P</Label>
+    </div>
+    <div class="flex items-center space-x-2">
+      <RadioGroupItem
+        id="option-two"
+        value="option-two"
+      />
+      <Label for="option-two">M</Label>
+    </div>
+    <div class="flex items-center space-x-2">
+      <RadioGroupItem
+        id="option-three"
+        value="option-three"
+      />
+      <Label for="option-three">G</Label>
+    </div>
+  </RadioGroup>
+</template>
