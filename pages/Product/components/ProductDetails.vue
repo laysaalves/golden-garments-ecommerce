c5830923f5539d89f3c91536734614f60f17a8e2
Laysa <laysa.developer@gmail.com>
2024-09-05 16:11:49 -0300

    feat(product pages): add discount function to products price

--- pages/Product/components/ProductDetails.vue
+++ pages/Product/components/ProductDetails.vue
@@ -7,8 +7,10 @@
     <h1 class="text-2xl">{{ product.name }}</h1>
   </div>
     <div class="flex flex-row items-center justify-center gap-2 mt-4 mb-4">
-      <p class="line-through opacity-20 text-lg">R$ {{ product.price }}</p>
-      <p class="text-2xl font-bold">R$ {{ product.price }}</p>
+      <div v-if="product.discountPercentage > 0">
+        <p class="line-through opacity-20 text-lg">R$ {{ product.price }}</p>
+      </div>
+      <p class="text-2xl font-bold">R$ {{ discountedPrice(+product.price, product.discountPercentage) }}</p>
       <Badge
         v-if="product.discountPercentage > 0"
         variant="secondary"
@@ -26,4 +28,10 @@ import { ArrowDown } from 'lucide-vue-next'
 const route = useRoute()
 const productId = route.query.productId
 const { data: products } = await useFetch(`/api/products/productDetails`, { query: { productId } })
+
+function discountedPrice(basePrice: number, discount: number): number {
+  const discountPercentage = basePrice * (discount / 100)
+  const newPrice = basePrice - discountPercentage
+  return newPrice
+}
 </script>
