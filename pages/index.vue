f479282fa50531caf793abdf2e2327ce1180a7aa
Laysa <laysa.developer@gmail.com>
2024-09-07 11:02:29 -0300

    feat(home page): add product link to responsive banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="lg:block hidden">
-    <img src="/banner-web.png" class="w-full" />
+    <NuxtLink
+      :to="{
+        path: `/product/productsPreCart`,
+        query: { productId: 'eff5abb4-14d3-41c0-b88e-6f92dd9765b4' },
+      }"
+    >
+      <img
+        src="/banner-web.png"
+        class="w-full"
+      />
+    </NuxtLink>
   </div>
   <div class="lg:hidden">
-    <img src="/banner-mobile.png" class="w-full" />
+    <NuxtLink
+      :to="{
+        path: `/product/productsPreCart`,
+        query: { productId: 'eff5abb4-14d3-41c0-b88e-6f92dd9765b4' },
+      }"
+    >
+      <img
+        src="/banner-mobile.png"
+        class="w-full"
+      />
+    </NuxtLink>
   </div>
-  <div class="flex lg:justify-center lg:items-center gap-6 overflow-auto [&::-webkit-scrollbar]:hidden pt-8">
+  <div
+    class="flex lg:justify-center lg:items-center gap-6 overflow-auto [&::-webkit-scrollbar]:hidden pt-8"
+  >
     <CategorySection />
   </div>
 </template>
-
