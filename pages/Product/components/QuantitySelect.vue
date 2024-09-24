cf34226247e620a5cbcbc330c133d8e188be3481
Laysa <laysa.developer@gmail.com>
2024-09-24 11:02:44 -0300

    feat: add increment and decrement to product quantity selector

--- pages/Product/components/QuantitySelect.vue
+++ pages/Product/components/QuantitySelect.vue
@@ -3,13 +3,17 @@
     <Button
       variant="link"
       class="size-8 p-0"
+      @click="decrement"
+      :disabled="count === 1"
     >
       <Minus class="text-yellow-500 w-4 h-4" />
     </Button>
-    <span>1</span>
+    <span>{{ count }}</span>
     <Button
       variant="link"
       class="size-8 p-0"
+      @click="increment"
+      :disabled="count === 3"
     >
       <Plus class="text-yellow-500 w-4 h-4" />
     </Button>
@@ -17,5 +21,20 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { Minus, Plus } from 'lucide-vue-next'
+
+const count = ref(1)
+
+const increment = () => {
+  if (count.value < 3) {
+    count.value++
+  }
+}
+
+const decrement = () => {
+  if (count.value > 1) {
+    count.value--
+  }
+}
 </script>
